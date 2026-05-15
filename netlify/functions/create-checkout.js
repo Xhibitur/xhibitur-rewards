2ec544ff83051549fe9eb38bcb6eceaa33caa492
Xhibitur <james.berry@xhibitur.com>
2026-05-15 16:09:27 -0400

    Create create-checkout.js

---
+++ netlify/functions/create-checkout.js
@@ -0,0 +1,38 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  if (event.httpMethod === "OPTIONS") {
+    return { statusCode: 200, headers, body: "" };
+  }
+
+  try {
+    const { priceId, email } = JSON.parse(event.body);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      customer_email: email,
+      line_items: [{ price: priceId, quantity: 1 }],
+      trial_period_days: 14,
+      success_url: "https://rewards.xhibitur.com/#/dashboard?upgraded=true",
+      cancel_url: "https://rewards.xhibitur.com/#/pricing",
+    });
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ url: session.url }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
