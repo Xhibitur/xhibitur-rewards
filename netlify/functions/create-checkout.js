d01f547bcb85a60b341559a8e7aca2feaf24d4b4
Xhibitur <james.berry@xhibitur.com>
2026-05-23 17:51:02 -0400

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -13,16 +13,14 @@ exports.handler = async (event) => {
   try {
     const { priceId, email } = JSON.parse(event.body);
 
-    console.log("Key prefix:", process.env.STRIPE_SECRET_KEY?.slice(0, 8));
-    console.log("Price ID:", priceId);
-    console.log("Email:", email);
-
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
       payment_method_types: ["card"],
       customer_email: email,
       line_items: [{ price: priceId, quantity: 1 }],
-      trial_period_days: 14,
+      subscription_data: {
+        trial_period_days: 14,
+      },
       success_url: "https://rewards.xhibitur.com/#/dashboard?upgraded=true",
       cancel_url: "https://rewards.xhibitur.com/#/pricing",
     });
